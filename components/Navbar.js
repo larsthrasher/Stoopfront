af18662bed646f3d030ff2575a2ce07f27fe6905
larsthrasher <thrasherlars@gmail.com>
2020-04-09 11:28:49 -0400

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,42 @@
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import ShareOutlined from "@material-ui/icons/ShareOutlined";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import BusinessTwoTone from "@material-ui/icons/BusinessTwoTone";
 import withStyles from "@material-ui/core/styles/withStyles";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = ({ classes, router, pageProps: { auth } }) => {
+  const { user = {} } = auth || {};
+
+
+  return (
+    <AppBar className={classes.appBar} position={router.pathname === '/' ?
+    "fixed" : "static"}>
+      <Toolbar>
+        <BusinessTwoTone className={classes.icon}/>
+        <Typography
+          variant="h5"
+          component="h1"
+          className={classes.toolbarTitle}
+        >
+          Stoopfront
+        </Typography>
+
+        {!user._id ? (
+          //Auth navigation
+          <div>
+            <Button>Profile</Button>
+            <Button variant='outlined'>Sign out</Button>
+          </div>
+        ) : (
+          <div>
+            <Button>Sign in</Button>
+            <Button>Sign up</Button>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  )
 };
 
 const styles = theme => ({
