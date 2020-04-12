5245abfc50761e2ead972c89dd92717cace1e7a9
larsthrasher <thrasherlars@gmail.com>
2020-04-12 14:31:16 -0400

    profile edit page

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,19 @@
-const mongoose = require('mongoose')
-const User = mongoose.model('User');
-const multer = require('multer');
-const jimp = require('jimp');
-
-
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const multer = require("multer");
+const jimp = require("jimp");
 
 exports.getUsers = async (req, res) => {
-  const users = await User.find().select('_id name email createdAt updatedAt');
+  const users = await User.find().select("_id name email createdAt updatedAt");
   res.json(users);
 };
 
 exports.getAuthUser = (req, res) => {
-  if(!req.isAuthUser) {
-   res.status(403).json({
-      message: "You're unauthenticated. Please sign in or sign up."
+  if (!req.isAuthUser) {
+    res.status(403).json({
+      message: "You are unauthenticated. Please sign in or sign up"
     });
-    return res.redirect('/signin')
+    return res.redirect("/signin");
   }
   res.json(req.user);
 };
@@ -24,9 +22,9 @@ exports.getUserById = async (req, res, next, id) => {
   const user = await User.findOne({ _id: id });
   req.profile = user;
 
-  const profileId = mongoose.Types.ObjectId(req.profile._id)
+  const profileId = mongoose.Types.ObjectId(req.profile._id);
 
-  if (req.user && profileId.equals(req.user._id)){
+  if (req.user && profileId.equals(req.user._id)) {
     req.isAuthUser = true;
     return next();
   }
@@ -42,47 +40,49 @@ exports.getUserProfile = (req, res) => {
   res.json(req.profile);
 };
 
-
 exports.getUserFeed = async (req, res) => {
-  const { following, _id } = req.profile
+  const { following, _id } = req.profile;
 
-  following.push(_id)
-  const users = await User.find({ _id: { $nin: following } })
-      .select('_id name avatar')
-  res.json(users)
+  following.push(_id);
+  const users = await User.find({ _id: { $nin: following } }).select(
+    "_id name avatar"
+  );
+  res.json(users);
 };
 
 const avatarUploadOptions = {
   storage: multer.memoryStorage(),
   limits: {
-    //Storing image files up to 2mb
-    filesSize: 1024 * 1024 * 2
+    // storing images files up to 1mb
+    fileSize: 1024 * 1024 * 1
   },
   fileFilter: (req, file, next) => {
-    if (file.mimetype.startsWith('image/')){
+    if (file.mimetype.startsWith("image/")) {
       next(null, true);
     } else {
       next(null, false);
     }
   }
-}
+};
 
-exports.uploadAvatar = multer(avatarUploadOptions).single('avatar');
+exports.uploadAvatar = multer(avatarUploadOptions).single("avatar");
 
 exports.resizeAvatar = async (req, res, next) => {
-  if(!req.file) {
+  if (!req.file) {
     return next();
   }
-  const extension = req.file.mimetype.split('/')[1]
-  req.body.avatar = `/static/uploads/avatars/${req.user.name}-${Date.now()}.${extension}`
-  const image = await jimp.read(req.file.buffer)
-  await image.resize(250, jimp.AUTO)
-  await image.write(`./${req.body.avatar}`)
+  const extension = req.file.mimetype.split("/")[1];
+  req.body.avatar = `/static/uploads/avatars/${
+    req.user.name
+  }-${Date.now()}.${extension}`;
+  const image = await jimp.read(req.file.buffer);
+  await image.resize(250, jimp.AUTO);
+  await image.write(`./${req.body.avatar}`);
   next();
 };
 
 exports.updateUser = async (req, res) => {
-  req.body.updateAt = new Date().toISOString();
+  req.body.updatedAt = new Date().toISOString();
   const updatedUser = await User.findOneAndUpdate(
     { _id: req.user._id },
     { $set: req.body },
@@ -94,22 +94,22 @@ exports.updateUser = async (req, res) => {
 exports.deleteUser = async (req, res) => {
   const { userId } = req.params;
 
-  if(!req.isAuthUser) {
+  if (!req.isAuthUser) {
     return res.status(400).json({
-      message: "You are not authorized to perform this action."
-    })
+      message: "You are not authorized to perform this action"
+    });
   }
-  const deletedUser = await User.findOneAndDelete({ _id: userId })
-  res.json(deletedUser)
+  const deletedUser = await User.findOneAndDelete({ _id: userId });
+  res.json(deletedUser);
 };
 
-exports.addFollowing = async(req, res, next) => {
+exports.addFollowing = async (req, res, next) => {
   const { followId } = req.body;
 
   await User.findOneAndUpdate(
     { _id: req.user._id },
-    { $push: { following: followId }}
-  )
+    { $push: { following: followId } }
+  );
   next();
 };
 
@@ -120,8 +120,8 @@ exports.addFollower = async (req, res) => {
     { _id: followId },
     { $push: { followers: req.user._id } },
     { new: true }
-  )
-  res.json(user)
+  );
+  res.json(user);
 };
 
 exports.deleteFollowing = async (req, res, next) => {
@@ -129,8 +129,8 @@ exports.deleteFollowing = async (req, res, next) => {
 
   await User.findOneAndUpdate(
     { _id: req.user._id },
-    { $pull: { following: followId }}
-  )
+    { $pull: { following: followId } }
+  );
   next();
 };
 
